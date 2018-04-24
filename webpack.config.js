54b2203262dd759c195160bbdd9c0331ed8dea29
thomas jouannic <thomas.jouannic@nomadeducation.fr>
2018-04-24 11:31:09 +0200

    chore(webpack): use "prod" mode by default

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     // TODO find a way to use devel mode locally and prod mode externally
     // Besides, don't invoke postinstall when adding new packages locally
-    //mode: "development"
+    mode: "production"
 };
