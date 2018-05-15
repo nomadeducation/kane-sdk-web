f8c2eeb85896bad7c2ef8f273778a8d42ec9589d
thomas jouannic <thomas.jouannic@nomadeducation.fr>
2018-05-15 16:58:02 +0200

    test(auth): init

--- test/identity/index.test.js
+++ test/identity/index.test.js
@@ -1,42 +1,42 @@
 const nomad = require("dist/test/sdk.node")();
 const mockServer = require("mockttp").getLocal();
+const faker = require("faker/locale/fr");
 const chai = require("chai");
 const expect = chai.expect;
 
-describe("Dummy test", function () {
+describe("Identity", function () {
     // use the same port as defined in webpack.test.js
     beforeEach(() => mockServer.start(3333));
     afterEach(() => mockServer.stop());
 
-    it("should return true", async function () {
-        await mockServer.get("/v2/users").thenReply(200, JSON.stringify([
+    it("should register one user", async function () {
+        const fakeUser = {
+            email: faker.internet.email(),
+            username: faker.internet.userName(),
+            first_name: faker.name.firstName(),
+            last_name: faker.name.lastName()
+        };
+
+        const responseObj = Object.assign(
             {
                 id: "676a14fc-5b67-4462-a1e5-dd4e3208b593",
-                first_name: "mr",
-                last_name: "robot",
-                email: "elliot@e-corp-usa.com",
-                created_at: "2018-03-09T18:25:47.369Z",
-                updated_at: "2018-03-09T19:11:28.798Z"
+                created_at: faker.date.recent(),
+                updated_at: faker.date.recent()
             },
-            {
-                id: "edb324b9-2fea-4765-83d1-d6ed6f2d3af5",
-                email: "johnj@example.com",
-                created_at: "2018-03-09T18:25:47.369Z",
-                updated_at: "2018-03-09T19:11:28.798Z"
-            }
-        ]));
+            fakeUser
+        );
 
-        const res = await nomad.users();
-        const users = res.data;
-        expect(users).to.be.an("array").that.has.lengthOf(2);
+        await mockServer.post("/v2/register").thenReply(200, JSON.stringify(responseObj));
 
-        for (const user of users) {
-            expect(user).to.include.all.keys(
-                "id",
-                "email",
-                "created_at",
-                "updated_at"
-            );
-        }
+        const newUser = await nomad.register(fakeUser);
+        expect(newUser).to.be.an("object").to.include.all.keys(
+            "id",
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "created_at",
+            "updated_at"
+        );
     });
 });
