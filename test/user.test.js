bcce84f9295a19260d6ee2a2a6f68b55e226ed32
thomas jouannic <thomas.jouannic@nomadeducation.fr>
2018-12-14 11:22:04 +0100

    test(user): init
    Using the testing units from the "index" test file.

---
+++ test/user.test.js
@@ -0,0 +1,87 @@
+const Nomad = require("dist/node");
+const account = require("./account");
+const faker = require("faker/locale/fr");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("User", function () {
+    describe("methods as a logged user", function () {
+        const client = new Nomad();
+        let newUser = {};
+
+        before(async () => {
+            const isConnected = await client.login(account.username, account.password);
+            expect(isConnected).to.be.true;
+        });
+
+        after(async () => {
+            const loggedOut = await client.logout();
+            expect(loggedOut).to.be.true;
+        });
+
+        it("should create a new user", async function () {
+            const fakeUser = {
+                email: `dummy.user+${faker.random.uuid()}@nomadeducation.fr`,
+                password: faker.internet.password(),
+                first_name: faker.name.firstName(),
+                last_name: faker.name.lastName()
+            };
+
+            newUser = await client.user.create(fakeUser);
+
+            expect(newUser).to.be.an("object").that.include.all.keys(
+                "id",
+                "created_at",
+                "updated_at"
+            );
+        });
+
+        it("should test the existence of the new user", async function () {
+            const doesExists = await client.user.exists(newUser.id);
+
+            expect(doesExists).to.be.a("boolean").that.is.true;
+        });
+
+        it("should update the created user", async function () {
+            const fakeInfos = {
+                first_name: faker.name.firstName(),
+                last_name: faker.name.lastName()
+            };
+
+            const updated = await client.user.update(newUser.id, fakeInfos);
+
+            expect(updated).to.be.a("boolean").that.is.true;
+        });
+
+        it("should disable the created user", async function () {
+            const disabled = await client.user.disable(newUser.id);
+
+            expect(disabled).to.be.a("boolean").that.is.true;
+        });
+
+        it("should delete the created user", async function () {
+            const removed = await client.user.remove(newUser.id);
+
+            expect(removed).to.be.a("boolean").that.is.true;
+        });
+
+        it("should check that the user doesn't exist", async function () {
+            const userId = "42";
+            const doesExists = await client.user.exists(userId);
+
+            expect(doesExists).to.be.a("boolean").that.is.false;
+        });
+    });
+
+    describe("methods as a logged user using the API key", function () {
+        // XXX no need to login before
+        const {apiKey} = account;
+
+        it("should check that there are some users still there!", async function () {
+            const apiClient = new Nomad({api_key: apiKey});
+            const {count} = await apiClient.user.metadata();
+
+            expect(count).to.be.a("number").that.is.at.least(1);
+        });
+    });
+});
