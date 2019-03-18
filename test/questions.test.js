6d87dcce82bb36d65752a09978924fdfff1483d3
thomas jouannic <thomas.jouannic@nomadeducation.fr>
2019-03-18 16:51:10 +0100

    feat(question): init with tests

---
+++ test/questions.test.js
@@ -0,0 +1,63 @@
+const Nomad = require("dist/node");
+const account = require("./account");
+const faker = require("faker/locale/fr");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("Questions", function () {
+    const client = new Nomad();
+    let newQuestion = {};
+    const questionName = `dummy question ${faker.random.uuid()}`;
+
+    before(async () => {
+        const isConnected = await client.login(account.username, account.password);
+        expect(isConnected).to.be.true;
+    });
+
+    after(async () => {
+        const loggedOut = await client.logout();
+        expect(loggedOut).to.be.true;
+    });
+
+    it("should create a new question", async function () {
+        const fakePost = {
+            title: questionName,
+            content_type: "question"
+        };
+
+        newQuestion = await client.questions.create(fakePost);
+
+        expect(newQuestion).to.be.an("object").that.include.all.keys(
+            "id",
+            "created_at"
+        );
+    });
+
+    it("should test the existence of the new question", async function () {
+        const doesExists = await client.questions.exists(newQuestion.id);
+
+        expect(doesExists).to.be.a("boolean").that.is.true;
+    });
+
+    it("should update the created question", async function () {
+        const fakeInfos = {
+            title: `changed ${questionName}`
+        };
+
+        const updated = await client.questions.update(newQuestion.id, fakeInfos);
+
+        expect(updated).to.be.a("boolean").that.is.true;
+    });
+
+    it("should delete the created question", async function () {
+        const removed = await client.questions.remove(newQuestion.id);
+
+        expect(removed).to.be.a("boolean").that.is.true;
+    });
+
+    it("should check that the question doesn't exist", async function () {
+        const doesExists = await client.questions.exists(newQuestion.id);
+
+        expect(doesExists).to.be.a("boolean").that.is.false;
+    });
+});
